76390809f2f9b435bf15f327bd15ee2d25d085f6
hons <70585552+honsda@users.noreply.github.com>
2022-03-05 09:18:21 +0700

    you saw nothing

--- web/script.js
+++ web/script.js
@@ -15,13 +15,11 @@ function sub(obj) {
     var file = obj.value;
     var fileName = file.split("\\");
     fname = fileName[fileName.length - 1];
-    document.myForm.submit();
-    event.preventDefault();
 }
 
 document.getElementById('mapeditorfile').addEventListener('change',function () {
             var fr = new FileReader();
-            fr.onload = function () {document.getElementById('showfile').innerHTML = `<p style="background-color:rgba(49, 49, 53, 0.5);padding:10px;">${fname}</p>${this.result};`};
+            fr.onload = function () {document.getElementById('showfile').innerHTML = `<p style="font-weight:bolder;background-color:rgba(49, 49, 53, 0.5);padding:10px;">${fname}</p>${this.result};`};
             fr.readAsText(this.files[0]);
         }
     );
